599ff113c504fc3630965027a8779189bb3507dd
EugeneAbramov <petzchner@gmail.com>
2021-06-24 14:15:54 +0300

    add ood expeririments with different models

--- ood_roc.py
+++ ood_roc.py
@@ -1,16 +1,36 @@
 import torch
-from quadrics.model import HSQuadricsModel
+import sys
+from sklearn.preprocessing import normalize
+import pandas as pd
+from tqdm import tqdm
+from ood_tests.rocauc import *
 from ood_tests.data_reader import read_ood, read_dataset
-from ood_tests.rocauc import rocauc_stats
+from quadrics.model import HSQuadricsModel
 
-QPATH = '/workspace/models/100_kPCA_quadrics_46.pth'
-N_QUADRICS = 200
-N_COMP = 512
-DEVICE = torch.device('cuda:0')
-alg_dist = True
+
+params = {
+    'quadrics': {
+        'n_samples_to_train': 'all', 
+        'QPATH': '/workspace/models/100_kPCA_quadrics_46.pth',
+        'N_QUADRICS': 200,
+        'N_COMP': 512,
+        'DEVICE': torch.device('cuda:0'),
+        'alg_dist': True
+    },
+    'OneClassSVM': {
+        'n_samples_to_train': 4000, 
+        'kernel': 'poly',
+        'degree': 3
+    },
+    'PCA': {
+        'n_samples_to_train': 100000,
+        'n_components': 170
+    }
+}
+
 
 config = dict()
-config['anime'] = {'path':'/workspace/data/anime.npy', 'n':235}
+config['anime'] = {'path':'/workspace/data/anime.npy', 'n': 235}
 config['cplfw'] = '/workspace/data/cplfw_retina_embs'
 config['our_ood'] = '/workspace/data/cplfw_outv2.json'
 config['mtcnn_ood'] = '/workspace/data/cplfw_intersection_out.json'
@@ -21,19 +41,45 @@ datasets = {'ms1m':'/workspace/data/ms1m_new_float32.npy',
             'ffhq':'/workspace/data/ffhq_embs',
             'calfw':'/workspace/data/calfw_embs'}
 
+# models we train
+models_we_use = ['OneClassSVM', 'quadrics', 'PCA']
+filename_to_save = 'results.csv'
 
-model = HSQuadricsModel(N_QUADRICS, dim=N_COMP)
-model.load(QPATH, map_location=DEVICE)
-model = model.to(DEVICE)
-ood_embs, id_embs = read_ood(config)
-m, s = rocauc_stats(model, id_embs, ood_embs, DEVICE, alg_dist=alg_dist)
-print(f'cplfw: mean {m}, std {s}')
-with open('results.txt', 'w+') as file:
-    file.write(f'cplfw: mean {m}, std {s}\n')
-for data in datasets:
-    dataset = datasets[data]
-    id_embs = read_dataset(dataset)
-    m, s = rocauc_stats(model, id_embs, ood_embs, DEVICE, alg_dist=alg_dist)
-    print(data + f': mean {m}, std {s}')
-    with open('results.txt', 'a+') as file:
-        file.write(data + f': mean {m}, std {s}\n')
+
+if __name__ == '__main__':
+    # read dataset to train on
+    data_to_train = normalize(np.load(datasets['megaface']))
+    
+    # load ood embeddings, cplfw with no outliers
+    ood_embs, cplfw_embs = read_ood(config)
+    
+    # prepare results table
+    iterator = iter([i for i in range(1000)])
+    results_table = pd.DataFrame(columns=['dataset', 'model', 'mean', 'std'])
+    
+    # train and load models
+    models_dict = {}
+    for model in models_we_use:
+        # fit a model, return a dict
+        models_dict[model] = train_model(model, data_to_train, params[model])
+    
+    if 'quadrics' in set(models_we_use):
+        # load quadrics model
+        model = HSQuadricsModel(params['quadrics']['N_QUADRICS'], dim=params['quadrics']['N_COMP'])
+        model.load(params['quadrics']['QPATH'], map_location=params['quadrics']['DEVICE'])
+        model = model.to(params['quadrics']['DEVICE'])
+        models_dict['quadrics'] = model
+    
+    # cplfw no distractors
+    for result in rocauc_experiments(cplfw_embs, ood_embs, models_dict, extra_params=params['quadrics']):
+        results_table.loc[next(iterator)] = ['cplfw'] + result
+    
+    for dataset_name, dataset_dir in tqdm(datasets.items(), desc="Dataset"):
+        in_distr_embs = read_dataset(dataset_dir)
+        for result in rocauc_experiments(in_distr_embs, ood_embs, models_dict, extra_params=params['quadrics']):
+            results_table.loc[next(iterator)] = [dataset_name] + result
+    
+    results_table.index = pd.MultiIndex.from_arrays([results_table.dataset.tolist(), results_table.model.tolist()])
+    results_table.to_csv(filename_to_save, index=False)
+                
+            
--- ood_tests/rocauc.py
+++ ood_tests/rocauc.py
@@ -1,8 +1,41 @@
-import numpy as np
 import torch
+import numpy as np
+from sklearn.svm import OneClassSVM
+from sklearn.decomposition import PCA
 from sklearn.metrics import roc_auc_score
 from tqdm import tqdm
 
+
+def train_model(model_type, data_to_train, model_params):
+    """Return a trained model"""
+    n_samples_to_train = model_params['n_samples_to_train']
+    model_params.pop('n_samples_to_train')
+    
+    if n_samples_to_train == 'all':
+        indices = [i for i in range(len(data_to_train))]
+    else:
+        try:
+            indices = np.random.choice(len(data_to_train), n_samples_to_train,
+                                       replace=False)
+        except ValueError:
+            indices = np.random.choice(len(data_to_train), n_samples_to_train)
+        
+    if model_type == 'quadrics':
+        pass
+ 
+    if model_type == 'OneClassSVM':
+        clf = OneClassSVM(**model_params).fit(data_to_train[indices])
+        return clf
+    
+    if model_type == 'KPCA':
+        pass
+    
+    if model_type == 'KPCA_rff':
+        pass
+    
+    if model_type == 'PCA':
+        pca = PCA(**model_params).fit(data_to_train)
+        return pca
 
 def build_quadratic_monoms(point):
     monoms = []
@@ -11,15 +44,15 @@ def build_quadratic_monoms(point):
         monoms.append(cur_monoms)
     return np.concatenate(monoms)
 
-def get_dists(model, embs, device, batch_size=257, alg_dist=False):
+def get_dists_quadrics_(model, embs, DEVICE='cpu', batch_size=257, alg_dist=False):
     N_steps = embs.shape[0] // batch_size + 1
     res = []
     with torch.no_grad():
         for i in range(N_steps):
             q = np.array([build_quadratic_monoms(e) for e in embs[i*batch_size:(i+1)*batch_size]])
             p = embs[i*batch_size:(i+1)*batch_size]
-            q = torch.from_numpy(q.astype('float32')).to(device)
-            p = torch.from_numpy(p.astype('float32')).to(device)
+            q = torch.from_numpy(q.astype('float32')).to(DEVICE)
+            p = torch.from_numpy(p.astype('float32')).to(DEVICE)
             values = torch.abs(model.get_values((q,p)))
             if alg_dist:
                 scores = values
@@ -29,24 +62,54 @@ def get_dists(model, embs, device, batch_size=257, alg_dist=False):
             res.append(scores.detach().cpu().numpy())
     return np.concatenate(res, axis=0)
 
-def get_rocauc(model, id_embs, ood_embs, device, batch_size=1500, alg_dist=False):
-    y = np.array([1]*len(id_embs) + [0]*len(ood_embs))
-    embs = np.concatenate((id_embs, ood_embs), axis=0)
-    dists = np.mean(get_dists(model, embs, device, 
-        batch_size=batch_size, alg_dist=alg_dist), axis=1)
-    d = roc_auc_score(1-y, dists)
-    return d
-
-def rocauc_stats(model, id_embs, ood_embs, device, 
-                lam=99, iterations=10, batch_size=1500, alg_dist=False):
-    id_num = lam*len(ood_embs)
-    if id_num >= len(id_embs):
-        mean = get_rocauc(model, id_embs, ood_embs, device, batch_size=batch_size, alg_dist=alg_dist)
-        std = 0.
-        return mean, std
+def get_dist(model, model_type, embs, extra_params=None):
+    """Insert extra_params in the dictionary format"""
     
-    res = []
-    for _ in tqdm(range(iterations)):
-        id_inds = np.random.choice(len(id_embs), id_num)
-        res.append(get_rocauc(model, id_embs[id_inds], ood_embs, device, batch_size=batch_size, alg_dist=alg_dist))
-    return np.mean(res), np.std(res)
+    if model_type == 'quadrics':
+        extra_params.pop('QPATH', None)
+        extra_params.pop('N_QUADRICS', None)
+        extra_params.pop('N_COMP', None)
+        return np.mean(get_dists_quadrics_(model, embs, **extra_params), axis=1)
+    if model_type == 'OneClassSVM':
+        return model.score_samples(embs)
+    if model_type == 'KPCA':
+        return None
+    if model_type == 'KPCA_rff':
+        return None
+    if model_type == 'PCA':
+        # use - for roc_auc_score function
+        emb_projected = np.dot(embs, model.components_.T)
+        emb_length = np.linalg.norm(emb_projected, axis=1)
+        return - np.sqrt(1 - emb_length**2)
+
+
+def get_rocauc(distances_id, distances_ood):
+    """Return roc score with 2 arrays as in-distribution
+       and out-of-distribution arrays respectively"""
+    y = np.array([1]*len(distances_id) + [0]*len(distances_ood))
+    return roc_auc_score(1-y, np.concatenate((distances_id, distances_ood), axis=0))
+
+def rocauc_experiments(in_distr_embs, ood_embs, models_dict, in_distr_fraction=4,
+                       n_experiments=2, filename='results_ood.csv', extra_params=None):
+    """Generates roc auc score for each model in models_dict"""
+    in_distr_num = in_distr_fraction*len(ood_embs)
+    
+    with tqdm(models_dict.keys(), desc="Model", leave=False) as t:
+        for model_type in t:
+            t.set_description('Model {}'.format(model_type))
+            experiment_results = []
+            for _ in range(n_experiments):
+                try:
+                    id_inds = np.random.choice(len(in_distr_embs), in_distr_num, replace=False)
+                except ValueError:
+                    id_inds = np.random.choice(len(in_distr_embs), in_distr_num)
+                
+                distances_in_distr = get_dist(models_dict[model_type], model_type, 
+                                              in_distr_embs[id_inds], extra_params)
+                distances_ood = get_dist(models_dict[model_type], model_type,
+                                         ood_embs, extra_params)
+                
+                experiment_results.append(get_rocauc(distances_in_distr, distances_ood))
+            
+            yield [model_type, np.mean(experiment_results), np.std(experiment_results)]
+
