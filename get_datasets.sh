c0da5e518b590a75188c1f50f04d9cbe27624b2f
EugeneAbramov <petzchner@gmail.com>
2022-02-03 14:01:20 +0300

    Correct folders

--- get_datasets.sh
+++ get_datasets.sh
@@ -11,6 +11,6 @@ cd $dir
 
 gdown -O "cplfw" --folder "${google_dir}/1YjZ-z8V3eZu00lixAo_w9HNMyKV2U6B6"
 gdown -O "ms1m" --folder "${google_dir}/1YdT9kcjNPaGyc3jXsrsT08S6ADv2lgQd"
-gdown -O "megaface" --folder "${google_dir}"
-#gdown --folder
-#gdown --folder
+gdown -O "megaface" --folder "${google_dir}/1zNZ_mOdQIrdXFc7i-1wsY118ZjuwwXh9"
+gdown -O "calfw" --folder "${google_dir}/1Q08dOMTICaQlUdeqVN7B1loc0o05c9Kj"
+gdown -O "flickr" --folder "${google_dir}/1OSDBuF6eGoDRFX8Se6m2TggfAYn-yIzO"
