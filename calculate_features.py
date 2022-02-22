e10c6d1c2c6c40fa031ec5f1fabf138faa3b536d
EugeneAbramov <petzchner@gmail.com>
2022-02-22 18:51:20 +0000

    add quadrics calculation to outlier detector notebook

--- calculate_features.py
+++ calculate_features.py
@@ -91,13 +91,13 @@ if __name__ == '__main__':
         if 'quadrics' in args.methods:
             clf = Quadrics(dist='dist2', device='gpu')
             clf.load(dir_models + '/Quadrics.pth')
-            dist = clf.get_distances(embeddings, dist='dist2', batch_size=257)
-            np.save(dir_dataset + '/Quadrics.npy', np.sum(dist, axis=1))
+            dist = clf.get_distances(normalize(embeddings), dist='dist2', batch_size=2024)
+            np.save(dir_dataset + '/quadrics_dist.npy', np.sum(dist, axis=1))
         if 'quadrics_alg' in args.methods:
             clf = Quadrics(dist='dist0', device='gpu')
             clf.load(dir_models + '/Quadrics_algebraic.pth')
-            dist = clf.get_distances(embeddings, dist='dist0', batch_size=257)
-            np.save(dir_dataset + '/Quadrics_algebraic.npy', np.sum(dist, axis=1))
+            dist = clf.get_distances(normalize(embeddings), dist='dist0', batch_size=2024)
+            np.save(dir_dataset + '/quadrics_alg_dist.npy', np.sum(dist, axis=1))
         if 'OneClassSVM' in args.methods:
             clf = pickle.load(open(dir_models + '/OneClassSVM.pickle', 'rb'))
             dist = get_dist(clf, 'OneClassSVM', embeddings, config_dict['normalize'])
